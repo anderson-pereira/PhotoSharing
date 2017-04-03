75171a069e440f6fb2863a689ec40fd136a36923
Anderson Pereira <anderson.rasec@outlook.com>
2017-04-02 22:38:28 -0300

    Model 3. Create the class PhotoSharingContext for the use EntityFramework.

---
+++ PhotoSharing/Models/PhotoSharingContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharing.Models
+{
+    public class PhotoSharingContext : DbContext
+    {
+        public DbSet<Photo> Photos { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
