ee2cec029e7f8b6b8588a208783608de257b9fc4
Anderson Pereira <anderson.rasec@outlook.com>
2017-05-03 23:39:47 -0300

    Model 6. Create the Interface IPhotoSharingContext.

---
+++ PhotoSharing/Models/IPhotoSharingContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoSharing.Models
+{
+    interface IPhotoSharingContext
+    {
+        //Propriedade é um tipo de método, já que os comportamentos são refletidos por métodos.
+        IQueryable<Photo> Photos { get; }
+        IQueryable<Comment> Comments { get; }
+        int SaveChanges();
+        T Add<T>(T entity) where T : class;
+        Photo FindPhotoById(int ID);
+        Comment FindCommentById(int ID);
+        T Delete<T>(T entity) where T : class;
+    }
+}
