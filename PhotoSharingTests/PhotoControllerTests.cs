907663b4d99645afecaaea72f19a2bb61f10b84c
Anderson Pereira <anderson.rasec@outlook.com>
2017-05-08 23:59:27 -0300

    Model 6. Created the new project Test - PhotoSharingTests.

---
+++ PhotoSharingTests/PhotoControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PhotoSharingTests
+{
+    [TestClass]
+    public class PhotoControllerTests
+    {
+        //Nome da classe igual a funcionalidade que será testada.
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
